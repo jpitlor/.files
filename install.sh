7b77520b31f3ad98e1ff9a8810f9404ff6b04a9e
Jordan Pitlor <jordan@pitlor.dev>
2022-08-29 18:16:53 -0400

    add some dotfiles and a quick installer

---
+++ install.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/piticent123/.files ~/.files
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+
+ln -S ~/.gitconfig ~/.files/.gitconfig
+ln -S ~/.tmux.conf ~/.files/.tmux.conf
+ln -S ~/.p10k.zsh ~/.files/.p10k.zsh
+ln -S ~/.gitconfig ~/.files/.gitconfig
+ln -S ~/.zshrc ~/.files/.zshrc
