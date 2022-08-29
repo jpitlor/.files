a10fb951417b7c8821f159cabc84a3a83fff6d8c
Jordan Pitlor <jordan@pitlor.dev>
2022-08-29 18:19:09 -0400

    fix install script

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 git clone https://github.com/piticent123/.files ~/.files
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 
-ln -S ~/.gitconfig ~/.files/.gitconfig
-ln -S ~/.tmux.conf ~/.files/.tmux.conf
-ln -S ~/.p10k.zsh ~/.files/.p10k.zsh
-ln -S ~/.gitconfig ~/.files/.gitconfig
-ln -S ~/.zshrc ~/.files/.zshrc
+ln -S ~/.gitconfig ~/.files/dotfiles/.gitconfig
+ln -S ~/.tmux.conf ~/.files/dotfiles/.tmux.conf
+ln -S ~/.p10k.zsh ~/.files/dotfiles/.p10k.zsh
+ln -S ~/.gitconfig ~/.files/dotfiles/.gitconfig
+ln -S ~/.zshrc ~/.files/dotfiles/.zshrc
